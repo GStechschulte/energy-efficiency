ad538ce7c86db5159099a2a394ba36d39da7f864
GStechschulte <stechschulteg@gmail.com>
2022-04-26 16:58:41 +0200

    kernel python functions for loading kernel design

--- src/codebase/kernel_utils.py
+++ src/codebase/kernel_utils.py
@@ -2,32 +2,23 @@ import torch
 import gpytorch
 
 
-def entsorgung_kernel():
+def entsorgung_kernel(time) -> int:
     """
     10 minute aggregation kernels
     """
 
-    ## Locally Periodic Kernels ##
-    period_constraint_short = gpytorch.constraints.Interval(0.08, 0.11) ## short term
-
     seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
-        gpytorch.kernels.PeriodicKernel(
-            period_length_constraint=period_constraint_short
-            )
+        gpytorch.kernels.PeriodicKernel()
         )
 
     seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
         gpytorch.kernels.RBFKernel()
-    )
+        )
 
     locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
 
-    period_constraint_long = gpytorch.constraints.Interval(0.18, 0.21) ## long term
-
     seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
-        gpytorch.kernels.PeriodicKernel(
-            period_length_constraint=period_constraint_long
-            )
+        gpytorch.kernels.PeriodicKernel()
         )
 
     seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
@@ -36,7 +27,6 @@ def entsorgung_kernel():
 
     locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
 
-    ## Variations Not Captured By The Trend ##
     local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
 
     local_variation = gpytorch.kernels.ScaleKernel(
@@ -45,10 +35,259 @@ def entsorgung_kernel():
         
     local_variation.alpha = local_variation_alpha
 
-    local_variation_2 = gpytorch.kernels.ScaleKernel(
+    if time == 10:
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+
+    elif time == 30:
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+
+
+def hauptluftung_kernel(time) -> int:
+    """
+    10 and 30 minute aggregation kernels
+    """
+    if time == 10:
+        seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+
+        locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+ 
+        local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+        local_variation = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+        
+        local_variation.alpha = local_variation_alpha
+
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+    
+    elif time == 30:
+        seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+
+        locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+        local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+        local_variation = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+        
+        local_variation.alpha = local_variation_alpha
+
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+
+
+def gesamtmessung_kernel(time) -> int:
+    """
+    10 and 30 minute aggregation kernels
+    """
+
+    if time == 30:
+        seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel()
+            )
+
+        locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
+
+        seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel()
+        )
+
+        locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+        local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+        local_variation = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+            
+        local_variation.alpha = local_variation_alpha
+
+        covar_module = locally_short_periodic + locally_long_periodic + local_variation
+
+        return covar_module
+    
+    elif time == 10:
+        seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel()
+            )
+
+        locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
+
+        seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.PeriodicKernel()
+            )
+
+        seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel()
+        )
+
+        locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+        local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+        local_variation = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RQKernel()
+            )
+            
+        local_variation.alpha = local_variation_alpha
+
+        covar_module = locally_short_periodic + locally_long_periodic + local_variation
+
+        return covar_module
+
+
+def eg_kernel(time) -> int:
+
+    seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
         gpytorch.kernels.RBFKernel()
         )
 
-    covar_module = locally_short_periodic + locally_long_periodic + local_variation
+    locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
 
-    return covar_module
+    seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RBFKernel()
+    )
+
+    locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+    local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+    local_variation = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RQKernel()
+        )
+        
+    local_variation.alpha = local_variation_alpha
+
+    if time == 10:
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+
+    elif time == 30:
+        covar_module = locally_short_periodic + locally_long_periodic + local_variation
+
+        return covar_module
+
+    else:
+        raise ValueError('Enter either 10 or 30 minute aggregation periods')
+
+
+def uv_eg_kernel(time) -> int:
+
+    seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RBFKernel()
+        )
+
+    locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
+
+    seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RBFKernel()
+    )
+
+    locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+    local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+    local_variation = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RQKernel()
+        )
+        
+    local_variation.alpha = local_variation_alpha
+
+    if time == 30:
+        covar_module = locally_short_periodic + locally_long_periodic + local_variation
+
+        return covar_module
+    if time == 10:
+        covar_module = locally_short_periodic + locally_long_periodic + local_variation
+
+        return covar_module
+
+
+def og_kernel(time) -> int:
+
+    seasonal_periodic_short = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_short = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RBFKernel()
+        )
+
+    locally_short_periodic = seasonal_periodic_short * seasonal_rbf_short
+
+    seasonal_periodic_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.PeriodicKernel()
+        )
+
+    seasonal_rbf_long = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RBFKernel()
+    )
+
+    locally_long_periodic = seasonal_periodic_long * seasonal_rbf_long
+
+    local_variation_alpha = gpytorch.priors.GammaPrior(1, 0.5)
+
+    local_variation = gpytorch.kernels.ScaleKernel(
+        gpytorch.kernels.RQKernel()
+        )
+        
+    local_variation.alpha = local_variation_alpha
+
+    if time == 30:
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
+    if time == 10:
+        covar_module = locally_long_periodic + local_variation
+
+        return covar_module
